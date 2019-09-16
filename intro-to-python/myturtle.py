f63aef7ff90ad8d2c6ef727ab1428f3d248e9150
tilly jay <W0424641@campus.nscc.ca>
2019-09-16 10:08:45 -0300

    py

---
+++ intro-to-python/myturtle.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.color("blue")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+
+turtle.right (45)
+turtle.forward(70)
+turtle.right(90)
+turtle.forward (70)
+
+
+turtle.right(45)
+turtle.forward(100)
+turtle.color("green")
+turtle.left(90)
+turtle.forward(100)
+turtle.back(100)
